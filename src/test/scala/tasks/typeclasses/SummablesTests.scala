f81957ca8f1879894d08df0cd170c35ff2fd0bb3
Mounir Samite <mounir.samite@gmail.com>
2025-04-24 17:40:05 +0200

    finish Ex4 Summables

---
+++ src/test/scala/tasks/typeclasses/SummablesTests.scala
@@ -0,0 +1,14 @@
+package tasks.typeclasses
+
+import org.junit.*
+import org.junit.Assert.*
+import tasks.typeclasses.Ex4Summables
+
+/* Tests should be clear, but note they are expressed independently of the 
+   specific implementation
+*/
+
+class SummablesTests:
+  
+
+  
